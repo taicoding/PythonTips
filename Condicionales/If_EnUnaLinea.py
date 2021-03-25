2d96463c371352f90909abdb861e39ec6f3766c8
Tai543 <tai.delgadillo@gmail.com>
2021-03-25 18:45:09 -0400

    If en una linea

---
+++ Condicionales/If_EnUnaLinea.py
@@ -0,0 +1,26 @@
+'''
+@author Tai543
+Sentencias condicionales: if
+'''
+'''
+¿Sabias que Python puedes expresar un IF en una sola 
+linea? 👩🏻‍🏫👩🏻‍💻
+Expresamos el If de la siguiente forma
+<Si es Verdad> if <condicion> else <Si es Falso>
+'''
+#Vamos a evaluar si un numero es par
+numero =800
+#Definimos la condicional en una sola linea
+print('PAR'if numero%2==0 else 'IMPAR')   
+print(numero%2) 
+#Resultado: PAR
+#Tambien podemos almacenar el resultado del IF en
+#una variable 
+numero = 807
+resultado = 'PAR'if numero%2==0 else 'IMPAR'
+print(resultado)
+print(numero%2)
+#Resultado: IMPAR
+#Los operadores ternarios en Python son mejor 
+#conocidos como expresiones condicionales
+
