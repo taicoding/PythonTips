50dc82e5d6d6d54b0b4b56ba29188cab8ad0bf43
Tai543 <tai.delgadillo@gmail.com>
2021-04-01 03:09:38 -0400

    Constantes

---
+++ TiposDeVariable/contsantes.py
@@ -0,0 +1,15 @@
+'''
+@author Tai543
+Tipos de Variables: Constantes
+Las contantes son tipos de variables 
+cuyo valor no cambia
+'''
+# Por convencion las constantes 
+# llevan nombres en mayusculas
+NOMBRE = 'TATIANA'
+NACIONALIDAD = 'BOLIVIA'
+PI = 3.1415
+SERVIDOR = 'http://192.168.0.1/'
+USUARIO = 'admin'
+# Recuerda usar esta notacion para 
+# que tu codigo sea entendible 👩🏻‍🏫👩🏻‍💻
