648fbacc287c8f8b52e030ac39493cf2380b7407
Tai543 <tai.delgadillo@gmail.com>
2024-09-03 14:53:46 -0400

    sets y listas

--- 02_estructuras_de_datos/01_listas/definicion.py
+++ 02_estructuras_de_datos/01_listas/definicion.py
@@ -2,12 +2,13 @@
 @author taicoding
 Formas de declarar una lista
 """
+
 # Sin elementos
 likes = []
 seguidores = list()
 # Con elementos
 letras = list(["P", "Y", "T", "H", "O", "N", 3.9, True])
-emociones = ["u_u", "uwu", "o_o", "uwu"]
+emociones = ["😌", "😖", "🤩", "😐"]
 # Bonus: ✨ Veamos el tipo ✨
 print(type(emociones))
 # R: <class 'list'>
