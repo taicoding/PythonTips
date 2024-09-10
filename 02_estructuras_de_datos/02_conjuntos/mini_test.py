8a19636be1c144ed42e4665c4312033cd75b9872
Tai543 <tai.delgadillo@gmail.com>
2024-09-09 21:50:24 -0400

    updated mini test sets

--- 02_estructuras_de_datos/02_conjuntos/mini_test.py
+++ 02_estructuras_de_datos/02_conjuntos/mini_test.py
@@ -1,9 +1,10 @@
 """
 @author taicoding
-Tema: Sets 
+Tema: Sets 🐍🍟
 ¿Cuál es el resultado? 👩🏻‍🏫👩🏻‍💻🐍
 """
+
 multiplos = {3, 6, 9, 12}
 impares = {1, 3, 5, 7, 9}
-print(multiplos & impares)
-print(type(multiplos))
+resultado = multiplos.intersection(impares)
+print(resultado)
